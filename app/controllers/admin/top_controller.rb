963ef9fd0877cca7a0734484f745c64f16f8d0d9
suganoo <suganoo2002@gmail.com>
2017-02-03 14:44:21 +0900

    add dashbord

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,5 +1,9 @@
 class Admin::TopController < ApplicationController
   def index
-    render action: 'index'
+    if current_administrator
+      render action: 'dashbord'
+    else
+      render action: 'index'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :staff do
+  namespace :staff, path: '' do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
     post 'session' => 'sessions#create', as: :session
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root 'top#index'
+    get 'login' => 'sessions#new', as: :login
+    post 'session' => 'sessions#create', as: :session
+    delete 'session' => 'sessions#destroy'
+    resources :staff_members
   end
 
   namespace :customer do
